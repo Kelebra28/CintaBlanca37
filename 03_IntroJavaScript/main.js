8397324f0dbae23943711c291596bec3be375f81
Kelebra <ricardokelebra@gmail.com>
2020-01-24 21:44:05 -0600

    Intro a JS

---
+++ 03_IntroJavaScript/main.js
@@ -0,0 +1,64 @@
+// alert("Hola soy cool!")
+
+//Declarar una variable
+// var nombre = valor 
+
+// Tipos de valores
+
+let numerico = 109867098765.5;
+let cadenaDeTexto = "hola, saludos!!!!";
+let booleano = true;
+let arreglo = [];
+let objeto = {};
+
+
+// operaciones
+
+let x = 10
+let y = 5
+
+// console.log( x + y)
+// console.log("Soy un texto")
+
+let suma = x + y
+// console.log(suma)
+
+let nombre = "Ricardo"
+let apellido = "Basurto"
+
+let nombreC = nombre + " " + apellido
+
+// console.log(nombreC);
+
+//prompt
+
+// let preguntaN = prompt('Dime tu nombre')
+
+// console.log(preguntaN)
+
+// 1.- Preguntar al usuario su edad, nombre y direccion, en mi consola debe de mostrarse de esta forma
+// nombre :
+// edad :
+// direccion:
+
+
+// 2.- Preguntar cuantos años tiene mi usiario y en la consola mostrar en que año nacio
+
+
+let preguntaNombre = prompt('Cual es tu nombre');
+let preguntaEdad = prompt('Que edad tienes');
+let preguntaDireccion = prompt('Donde vives')
+
+console.log("Nombre : " + preguntaNombre);
+console.log("Edad : " + preguntaEdad);
+console.log("Direccion : " + preguntaDireccion);
+
+// let  valorAnio = Number(preguntaEdad)
+console.log(typeof(preguntaEdad));
+let resta = 2019 - Number(preguntaEdad)
+
+console.log(resta);
+
+
+
+
